ef48c9ddf2562ee54fd3fa13916d0311a29ef036
Rob Loach <robloach@gmail.com>
2013-02-24 12:13:28 -0500

    Add Component support to Vegas

---
+++ component.js
@@ -0,0 +1,35 @@
+{
+  "name": "vegas",
+  "repo": "jaysalvat/vegas",
+  "description": "Vegas is a jQuery plugin to add beautiful fullscreen backgrounds and slideshows to your webpages.",
+  "version": "1.3.1",
+  "keywords": ["jquery"],
+  "dependencies": {
+    "component/jquery": "*"
+  },
+  "development": {},
+  "scripts": [
+    "jquery.vegas.js"
+  ],
+  "styles": [
+    "jquery.vegas.css"
+  ],
+  "files": [
+    "overlays/01.png",
+    "overlays/02.png",
+    "overlays/03.png",
+    "overlays/04.png",
+    "overlays/05.png",
+    "overlays/06.png",
+    "overlays/07.png",
+    "overlays/08.png",
+    "overlays/09.png",
+    "overlays/10.png",
+    "overlays/11.png",
+    "overlays/12.png",
+    "overlays/13.png",
+    "overlays/14.png",
+    "overlays/15.png",
+    "images/loading.gif"
+  ]
+}
